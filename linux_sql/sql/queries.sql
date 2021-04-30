19140032b061a20818e437c33384b2e151a11014
Deepak Elango <deelango96@gmail.com>
2021-04-30 04:18:51 +0000

    Version 1

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -14,14 +14,26 @@
         cpu_number,
         id
    ORDER BY
-        totalmem [DESC];
+        totalmem DESC;
 
 -- Average memory usage: this query will give data on memory used in 5 min intervals in percent
--- The column will consist of host_id, host_name, timestamp, avgerate memory used in percent
+-- The column will consist of host_id, host_name, timestamp, average memory used in percent
     SELECT
-         usage.host_id,
-         info.hostname,
-         date_trunc('hour', usage."timestamp") + date_part('minute', usage."timestamp"):: int / 5 * interval '5 min' AS clock,
-         AVG((info.totalmem - usage.memory_free/info.totalmem)*100) AS avg_used_mem_percentage
-
-    FROM host_usage usage, host_info info;
+         host_id,
+         hostname,
+         date_trunc('hour', current_timestamp) + date_part('minute', current_timestamp):: int / 5 * interval '5 min' AS clock,
+         AVG(((info.totalmem - usage.memory_free)*100/info.totalmem)) AS avg_used_mem_percentage
+    FROM host_usage as usage, host_info as info
+    WHERE usage.host_id = info.id
+    GROUP BY
+         current_timestamp,
+         hostname,
+         host_id;
+-- Detect host failure: This query will detect host failures if the host_usage table inserts less than 3 data points within a 5 min interval
+    SELECT
+         host_id,
+         date_trunc('hour', current_timestamp) + date_part('minute', current_timestamp):: int / 5 * interval '5 min' AS clock
+    FROM host_usage
+    GROUP BY
+        host_id
+    HAVING COUNT(*) < 3;
