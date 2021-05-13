e23cf977561ac12e9a213807556889c6745653dc
Deepak Raj Elango <deelango96@gmail.com>
2021-05-13 17:20:56 -0400

    Update RegexExcImp.java

--- core_java/grep/ca/jrvs/apps/practice/RegexExcImp.java
+++ core_java/grep/ca/jrvs/apps/practice/RegexExcImp.java
@@ -1,12 +1,10 @@
-package ca.jrvs.apps.practice;
-
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class RegexExcImp implements RegexExc {
 
     public boolean matchJpeg(String filename) {
-        return filename.matches("\\.+[j]+[p]+(g|eg)");
+        return filename.matches("([a-z]|[A-Z])+\\.+[j]+[p]+(g|eg)");
     }
 
     public boolean matchIp(String ip) {
@@ -16,7 +14,7 @@ public class RegexExcImp implements RegexExc {
     public boolean isEmptyLine(String line) {
         return line.matches("^\\s*$");
     }
-    
+
     public static void main(String[] args) {
         String ipExample = "8.8.8.8";
         String imgFilename = "abc.jpeg";
