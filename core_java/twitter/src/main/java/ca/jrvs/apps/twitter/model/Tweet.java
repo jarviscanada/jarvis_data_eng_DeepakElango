d1181ba6df90cd7e2f24495a02c234fed0a8697b
deelango <deelango96@gmail.com>
2021-05-31 15:14:37 -0400

    update

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Tweet.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Tweet.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-import java.awt.*;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "created_at",
