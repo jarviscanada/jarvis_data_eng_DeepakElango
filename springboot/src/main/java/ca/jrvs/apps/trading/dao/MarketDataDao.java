f841af91904fef133d6fc2067f5bbb4e7cda3cbe
deelango <deelango96@gmail.com>
2021-06-11 18:33:25 -0400

    update working
    Everything is working now, maven and the libraries are back to normal

--- springboot/src/main/java/ca/jrvs/apps/trading/dao/MarketDataDao.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/MarketDataDao.java
@@ -1,12 +1,26 @@
 package ca.jrvs.apps.trading.dao;
 
-import ca.jrvs.apps.trading.model.config.*;
-import ca.jrvs.apps.trading.model.domain.*;
-import com.sun.org.slf4j.internal.*;
-import org.springframework.beans.factory.annotation.*;
+import ca.jrvs.apps.trading.model.config.MarketDataConfig;
+import ca.jrvs.apps.trading.model.domain.IexQuote;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public class MarketDataDao implements CrudRepository<IexQuote, String> {
+public abstract class MarketDataDao implements CrudRepository<IexQuote, String> {
 
     private static final String IEX_BATCH_PATH = "/stock/market/batch?symbols=%s&types=quote&token=";
     private final String IEX_BATCH_URL;
