c2ca18802568bedeea40ca7e3a56670c2a09451d
deelango <deelango96@gmail.com>
2021-07-08 17:11:55 -0400

    Update data_explore.sql

--- python_data_analytics/python_data_analytics/psql/data_explore.sql
+++ python_data_analytics/python_data_analytics/psql/data_explore.sql
@@ -12,3 +12,12 @@ YOUR QUERY GOES HERE
 -- number of clients (e.g. unique client ID)
 YOUR QUERY GOES HERE
 
+-- invoice date range
+
+-- number of SKU/merchants
+
+-- Calculate average invoice amount excluding invoices with a negative amount
+
+-- Calculate total revenue
+
+-- Calculate total revenue by YYYYMM
