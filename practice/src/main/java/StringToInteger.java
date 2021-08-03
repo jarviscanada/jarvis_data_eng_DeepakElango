71b521d966df1fddf4c060b6721d8a751a7a54a7
deelango <deelango96@gmail.com>
2021-08-03 15:30:49 -0400

    rotate string

---
+++ practice/src/main/java/StringToInteger.java
@@ -0,0 +1,14 @@
+/**
+ * Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer
+ * (similar to C/C++'s atoi function).
+ *
+ *
+ */
+
+public class StringToInteger {
+
+    public int myAtoi(String s) {
+        return 0;
+    }
+
+}
