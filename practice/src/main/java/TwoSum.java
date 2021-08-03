0267b8258fc081bd0f79a0bc8242d71a1600fd16
deelango <deelango96@gmail.com>
2021-08-03 11:03:03 -0400

    two sum

---
+++ practice/src/main/java/TwoSum.java
@@ -0,0 +1,30 @@
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such
+ * that they add up to target.
+ *
+ * <p>Example 1:
+ *
+ * <p>Input: nums = [2,7,11,15], target = 9 Output: [0,1] Output: Because nums[0] + nums[1] == 9, we
+ * return [0, 1].
+ *
+ * <p>You may assume that each input would have exactly one solution, and you may not use the same
+ * element twice.
+ */
+
+import java.util.*;
+
+public class TwoSum {
+  public static int[] bruteForce(int[] arr, int target) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr.length; j++) {
+        if ((arr[i] + arr[j]) == target) {
+          return new int[] {i, j};
+        }
+      }
+    }
+    return null;
+  }
+
+
+
+}
