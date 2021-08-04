c56750da42741ca8a7ada5a7d71b2a1e32fe3749
deelango <deelango96@gmail.com>
2021-08-04 10:37:07 -0400

    palindrome done

--- practice/src/main/java/Palindrome.java
+++ practice/src/main/java/Palindrome.java
@@ -2,14 +2,21 @@
  * Given a string s, determine if it is a palindrome, considering only alphanumeric characters and
  * ignoring cases.
  *
- * Example 1:
+ * <p>Example 1:
  *
  * <p>Input: s = "race a car" Output: false Explanation: "raceacar" is not a palindrome.
  */
-
 public class Palindrome {
-    public static boolean isPalindrome(String s) {
-
+  public static boolean isPalindrome(String s) {
+    char[] chars = s.toCharArray();
+    int j = chars.length - 1;
+    for (int i = 0; i < chars.length; i++) {
+      if (chars[i] != chars[j]) {
         return false;
+      } else {
+        j--;
+      }
     }
+    return true;
+  }
 }
--- practice/src/test/java/PalindromeTest.java
+++ practice/src/test/java/PalindromeTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.*;
 public class PalindromeTest {
   @Test
   public void rotateString() {
-    String test1 = "race a car";
+    String test1 = "racecar";
+    String test2 = "race a car";
     assertTrue(Palindrome.isPalindrome(test1));
+    assertFalse(Palindrome.isPalindrome(test2));
   }
 }
