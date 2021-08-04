7a843a93af7276459a78fbc993afaa26705df362
deelango <deelango96@gmail.com>
2021-08-04 12:25:03 -0400

    swap two numbers done

---
+++ practice/src/test/java/SwapTwoNumbersTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SwapTwoNumbersTest {
+    @Test
+    public void rotateString() {
+        int test1 = 1;
+        int test2 = 2;
+        assertEquals(SwapTwoNumbers.swap(test1,test2), new int[]{1, 2});
+    }
+}
